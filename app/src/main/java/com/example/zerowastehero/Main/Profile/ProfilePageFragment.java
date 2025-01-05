248d62960bf9f8660f72f40797d0c45156be71df
Amir Hakim <muhdamirhakim2411@gmail.com>
2025-01-06 05:43:19 +0800

    updated visuals

--- app/src/main/java/com/example/zerowastehero/Main/Profile/ProfilePageFragment.java
+++ app/src/main/java/com/example/zerowastehero/Main/Profile/ProfilePageFragment.java
@@ -65,7 +65,7 @@ public class ProfilePageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_profilepage, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
         // Initialize view
         IBChallengesView = view.findViewById(R.id.IBChallengesView);
